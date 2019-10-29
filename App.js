e01ce2bd76bb9a9d94e1134f8d63ce06f1adea5d
Sherbet <Sherbet@Czarinas-MacBook-Air-3.local>
2019-10-29 22:38:29 +0800

    coordinates now appear on screen on button push

--- App.js
+++ App.js
@@ -3,19 +3,30 @@ import { StyleSheet, Text, View } from "react-native";
 import GetLocation from "./components/GetLocation";
 
 export default class App extends React.Component {
+  state = {
+    latitude: 0,
+    longitude: 0
+  };
+
   getLocationHandler = () => {
     console.log("pressed button");
     navigator.geolocation.getCurrentPosition(
       position => {
-        console.log(position);
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude
+        });
       },
       err => console.log(err)
     );
   };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Hello World!</Text>
+        <Text>Latitude: {this.state.latitude}</Text>
+        <Text>Longitude: {this.state.longitude}</Text>
+
         <GetLocation onGetLocation={this.getLocationHandler} />
       </View>
     );
