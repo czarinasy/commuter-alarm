631e57447d7fb70f09e9ecc465b74cbc800dede0
Sherbet <czarinasy17@gmail.com>
2019-11-15 10:49:22 +0800

    added current location's coordinates to Tracker screen

--- screens/Tracker.js
+++ screens/Tracker.js
@@ -1,11 +1,31 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import GetLocation from "../components/GetLocation";
 
 class Tracker extends React.Component {
+  state = {
+    latitude: 0,
+    longitude: 0
+  };
+  getLocationHandler = () => {
+    console.log("pressed button");
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude
+        });
+      },
+      err => console.log(err)
+    );
+  };
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.text}>Tracker</Text>
+        <Text>Latitude: {this.state.latitude}</Text>
+        <Text>Longitude: {this.state.longitude}</Text>
+        <GetLocation onGetLocation={this.getLocationHandler} />
       </View>
     );
   }
