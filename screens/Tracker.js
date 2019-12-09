01ece9d19ec43e5aa204927b5981b702a189b489
Sherbet <czarinasy17@gmail.com>
2019-12-09 13:36:19 +0800

    eta logs on the console but implementation is pretty gross

--- screens/Tracker.js
+++ screens/Tracker.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import GetLocation from "../components/GetLocation";
+import { GOOGLE_API_KEY } from "../API_KEYS";
 
 function degrees_to_radians(degrees) {
   var pi = Math.PI;
-  console.log(degrees * (pi / 180));
+  //console.log(degrees * (pi / 180));
   return degrees * (pi / 180);
 }
+
 class Tracker extends React.Component {
   state = {
     current: {
@@ -16,7 +18,8 @@ class Tracker extends React.Component {
     destination: {
       latitude: 0,
       longitude: 0
-    }
+    },
+    eta: 0
   };
   componentDidMount() {
     navigator.geolocation.getCurrentPosition(
@@ -37,12 +40,35 @@ class Tracker extends React.Component {
       }
     });
   }
+  getETA() {
+    //console.log("getETA");
+    //var orig = "14.6404507,121.0737046";
+    var orig = this.state.current.latitude + "," + this.state.current.longitude;
+    //var dest = "14.6368585,121.0744894";
+    var dest =
+      this.state.destination.latitude + "," + this.state.destination.longitude;
+    var url =
+      "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=" +
+      orig +
+      "&destinations=" +
+      dest +
+      "&key=" +
+      GOOGLE_API_KEY();
+    //console.log(url);
+    const fetchDistMat = async () => {
+      const response = await fetch(url);
+      const json = await response.json();
+      console.log(json);
+    };
+
+    fetchDistMat();
+  }
   inRadius() {
     cLat = degrees_to_radians(this.state.current.latitude);
     cLong = degrees_to_radians(this.state.current.longitude);
     dLat = degrees_to_radians(this.state.destination.latitude);
     dLong = degrees_to_radians(this.state.destination.longitude);
-    console.log(cLat);
+    //console.log(cLat);
     difLat = cLat - dLat;
     difLong = cLong - dLong;
 
@@ -53,7 +79,7 @@ class Tracker extends React.Component {
     r = 6371;
     val = c * r;
     radius = 0.5;
-    console.log(val);
+    //console.log(val);
     if (val <= radius) {
       return "Inside the area";
     } else {
@@ -72,6 +98,7 @@ class Tracker extends React.Component {
         <Text>{this.state.destination.latitude}</Text>
         <Text>{this.state.destination.longitude}</Text>
         <Text>{this.inRadius()}</Text>
+        {this.getETA()}
       </View>
     );
   }
